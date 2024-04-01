dce3d0f8b5a84c7b41ce018121469ca18a485f5e
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-04-01 22:50:17 +0530

    update mainpage

--- app/index.js
+++ app/index.js
@@ -1,48 +1,54 @@
 import { useState } from "react";
 
-import { View, ScrollView, SafeAreaView } from "react-native";
+import { View, ScrollView, SafeAreaView, Text, Image, Button, TouchableOpacity } from "react-native";
 import { Stack, useRouter } from "expo-router";
+import { useNavigation } from '@react-navigation/native'
 
 import { icons, images } from '../constants';
 import { Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
 
 const Home = () => {
+    const navigation = useNavigation();
     const router = useRouter();
     const [searchTerm, setSearchTerm] = useState("")
 
+    const handlePress = () => {
+        navigation.navigate('main');
+    }
+
     return (
-        <SafeAreaView className='flex bg-[#FAFAFC]'>
+        <SafeAreaView className='flex bg-[#ffffff] w-full h-full'>
             <Stack.Screen
                 options={{
                     headerShadowVisible: false,
-                    headerLeft: () => (
-                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" className='bg-[#83829A] text-white' />
-                    ),
-                    headerRight: () => (
-                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
-                    ),
                     headerTitle: ""
                 }}
-                className='bg-[#FAFAFC]'
             />
 
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View className='flex-1 p-4'>
-
-                    <Welcome
-                        searchTerm={searchTerm}
-                        setSearchTerm={setSearchTerm}
-                        handleClick={() => {
-                            if (searchTerm) {
-                                router.push(`/search/${searchTerm}`)
-                            }
-                        }}
-                    />
-                    <Popularjobs />
-                    <Popularjobs />
-                    <Popularjobs />
-                </View>
-            </ScrollView>
+            <View className='w-full flex items-center mb-12'>
+                <Text className='text-2xl font-bold text-[#0a0f17] mb-4'>Welcome to,</Text>
+                <Image
+                    source={require("../assets/images/InternHub.png")}
+                    className="w-[170px] h-[26px] flex justify-center items-center"
+                />
+            </View>
+            <View className='bg-white w-full p-4 flex items-center justify-center gap-4'>
+                <Image
+                    source={require("../assets/images/login.png")}
+                    className="w-[355px] h-[365px] flex justify-center items-center m-2"
+                />
+            </View>
+            <View className='w-full p-4 flex items-center mt-10'>
+                <Text className='text-lg font-bold text-[#0a0f17] text-center mb-8'>Begin your internship journey now. Click 'Get Started' to explore our homepage.</Text>
+                <TouchableOpacity
+                    className='flex justify-start mb-10 bg-[#0a0f17] text-white rounded-full px-6 py-3'
+                    onPress={handlePress}
+                >
+                    <Text className="text-xl font-bold text-center text-white">
+                        Get Started
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
